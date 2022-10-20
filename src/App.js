33940912f5d1be2ebca2c7ffe0aeec6d633dbf9d
Filip <filip.trifunovic23@gmail.com>
2022-10-20 16:42:55 +0200

    setting states, getting images

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo.js";
+
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setNewImages(false);
+      setLoading(false);
+    } catch (error) {
+      setNewImages(false);
+
+      setLoading(false);
+    }
+  };
+
+  return <h2>stock photos starter</h2>;
+}
+
+export default App;
